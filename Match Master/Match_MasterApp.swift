7169cc74101f047e3ab40ddd7b610c8dc57c7e7e
Surya Sendhilraj <surya.sendhiraj@gmail.com>
2022-01-14 02:15:10 -0500

    Initial Commit

---
+++ Match Master/Match_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match_MasterApp.swift
+//  Match Master
+//
+//  Created by Surya Sendhilraj on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct Match_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
